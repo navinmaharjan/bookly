1d688ad10613a6ca721ae0555135623cc7365b21
Navin Maharjan <129166934+navinmaharjan@users.noreply.github.com>
2023-08-02 21:50:48 +0545

    basic UI design on Log In & Regsiter Page

--- client/src/pages/register/index.js
+++ client/src/pages/register/index.js
@@ -21,44 +21,79 @@ const SignupSchema = Yup.object().shape({
 });
 
 const Register = () => (
-    <div>
-        <h1>Signup</h1>
-        <Formik
-            initialValues={{
-                firstName: '',
-                lastName: '',
-                password: '',
-                email: '',
-            }}
-            validationSchema={SignupSchema}
-            onSubmit={values => {
-                // same shape as initial values
-                console.log(values);
-            }}
-        >
-            {({ errors, touched }) => (
-               
-                    <Form>
-                        <Field name="firstName" placeholder="first name" className=" bord" />
-                        {errors.firstName && touched.firstName ? (
-                            <div>{errors.firstName}</div>
-                        ) : null}
-                        <Field name="lastName" placeholder="last name" />
-                        {errors.lastName && touched.lastName ? (
-                            <div>{errors.lastName}</div>
-                        ) : null}
-                        <Field name="password" type="password" placeholder="password" />
-                        {errors.lastName && touched.lastName ? (
-                            <div>{errors.lastName}</div>
-                        ) : null}
-                        <Field name="email" type="email" />
-                        {errors.email && touched.email ? <div>{errors.email}</div> : null}
-                        <button type="submit">Submit</button>
-                    </Form>
-               
-            )}
-        </Formik>
-    </div>
+    <>
+        <div className='bg-blue-900'>
+            <div className=' container mx-auto h-14 flex items-center text-white text-xl font-semibold'>
+                <h1 className='tracking-wider'>Bookly.com</h1>
+            </div>
+
+        </div>
+
+
+        <div className='w-full flex justify-center items-center mt-16'>
+
+            <div>
+                <div className='text-3xl text-gray-500 text-center'>
+                    <h1>Sign up/Create an account</h1>
+                </div>
+
+                <Formik
+                    initialValues={{
+                        firstName: '',
+                        lastName: '',
+                        password: '',
+                        email: '',
+                    }}
+                    validationSchema={SignupSchema}
+                    onSubmit={values => {
+                        // same shape as initial values
+                        console.log(values);
+                    }}
+                >
+                    {({ errors, touched }) => (
+                        <Form className='flex flex-col p-8 border-2 gap-5 w-[400px] mt-8 rounded-xl'>
+
+                            <Field name="firstName" placeholder="First name" className=" border p-2" />
+                            {errors.firstName && touched.firstName ? (
+                                <div>{errors.firstName}</div>
+                            ) : null}
+
+
+                            <Field name="lastName" placeholder="Last name" className="border p-2" />
+                            {errors.lastName && touched.lastName ? (
+                                <div>{errors.lastName}</div>
+                            ) : null}
+                            <Field name="email" type="email" placeholder="Email" className="border p-2" />
+                            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+
+                            <Field name="password" type="password" placeholder="Password" className="border p-2" />
+                            {errors.lastName && touched.lastName ? (
+                                <div>{errors.lastName}</div>
+                            ) : null}
+                            <div className='text-center mt-4'>
+                                <button type="submit" className='bg-blue-400 px-4 p-2  rounded-lg text-white w-2/5 transition duration-300 hover:bg-blue-200 hover:text-blue-900 uppercase font-semibold tracking-wide'>Submit</button>
+                            </div>
+                            <div className='text-xs text-center border-t border-b py-2 mt-4'>
+                                <p>By signing in or creating an account, you agree with our <span className='text-blue-900'>Terms & Conditions</span> and <span className='text-blue-900'>Privacy Statement</span></p>
+                            </div>
+                            <div className='text-xs text-center py-2'>
+                                <p>All rights reserved.
+                                    Copyright 2023 – Bookly.com™ </p>
+                            </div>
+
+                        </Form>
+                    )}
+                </Formik>
+            </div>
+
+        </div>
+
+
+
+    </>
+
+
+
 );
 
 export default Register
